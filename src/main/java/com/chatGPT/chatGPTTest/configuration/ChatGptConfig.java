48473db6a3eb7a92055f3e9b506b50e97c24190a
VijendraSingh29 <73494922+VijendraSingh29@users.noreply.github.com>
2023-05-21 22:20:26 +0530

    Update ChatGptConfig.java

--- src/main/java/com/chatGPT/chatGPTTest/configuration/ChatGptConfig.java
+++ src/main/java/com/chatGPT/chatGPTTest/configuration/ChatGptConfig.java
@@ -4,7 +4,8 @@ public class ChatGptConfig {
 
 public static final String AUTHORIZATION = "Authorization";
     public static final String BEARER = "Bearer ";
-    public static final String API_KEY = "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";
+    //Add open ai key here 
+    public static final String API_KEY = "";
     public static final String MODEL = "text-davinci-003";
     public static final Integer MAX_TOKEN = 300;
     public static final Double TEMPERATURE = 0.0;
